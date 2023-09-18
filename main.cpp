29ed4175683926427894ba37bee8bd20754fbb01
FrancoCasavecchia <128827804+FrancCasavecchiaUCC@users.noreply.github.com>
2023-09-18 12:32:55 -0300

    Added cmake, update main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../Cola/Cola.h"
+#include "../Pila/Pila.h"
+#include "../Lista/Lista.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
